a9dd7c316e18f70e470de7e085d0ab5b4474ccdb
Nikhil S P <112267674+nikhilsp242@users.noreply.github.com>
2023-09-21 14:37:27 +0530

    Time: 15 ms (96.57%), Space: 90.5 MB (5.83%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        nums1.insert(nums1.end(),nums2.begin(),nums2.end());
-        sort(nums1.begin(),nums1.end());
+        vector<int> merged;
+        int i=0,j=0;
+        
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<nums2[j]) merged.push_back(nums1[i++]);
+            else merged.push_back(nums2[j++]);
+        }
+
+        while(i<nums1.size()) merged.push_back(nums1[i++]);
+
+        while(j<nums2.size()) merged.push_back(nums2[j++]);
+
         double median;
-        if(nums1.size() % 2 == 1){
-            median = nums1[nums1.size()/2];
-            return median;
-        }
-        else{
-            median = nums1[(nums1.size()-1)/2] + nums1[nums1.size()/2];
-            return median/2;
-        }
+        int n = merged.size();
+
+        if(n%2 == 1) median  = merged[n/2];
+        else median = (merged[(n-1)/2] + merged[n/2]) / 2.0;
+
+        return median;
     }
 };
